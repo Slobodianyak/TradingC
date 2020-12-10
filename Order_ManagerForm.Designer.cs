bc0d52036da0fcf5b2f9809a5222a41e2a25b178
Slobodianyak <63192578+Slobodianyak@users.noreply.github.com>
2020-12-10 23:34:33 +0200

    Add files via upload

---
+++ Order_ManagerForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace CourseProjectWF
+{
+    partial class Order_ManagerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Order_ManagerLoginText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Order_ManagerSignUPFormLink = new System.Windows.Forms.LinkLabel();
+            this.Order_ManagerLoginButton = new System.Windows.Forms.Button();
+            this.Order_ManagerPasswordText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(43, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Order_ManagerLoginText
+            // 
+            this.Order_ManagerLoginText.Location = new System.Drawing.Point(186, 73);
+            this.Order_ManagerLoginText.Name = "Order_ManagerLoginText";
+            this.Order_ManagerLoginText.Size = new System.Drawing.Size(154, 22);
+            this.Order_ManagerLoginText.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(30, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // Order_ManagerSignUPFormLink
+            // 
+            this.Order_ManagerSignUPFormLink.AutoSize = true;
+            this.Order_ManagerSignUPFormLink.Location = new System.Drawing.Point(103, 275);
+            this.Order_ManagerSignUPFormLink.Name = "Order_ManagerSignUPFormLink";
+            this.Order_ManagerSignUPFormLink.Size = new System.Drawing.Size(158, 17);
+            this.Order_ManagerSignUPFormLink.TabIndex = 4;
+            this.Order_ManagerSignUPFormLink.TabStop = true;
+            this.Order_ManagerSignUPFormLink.Text = "Don\'t have an account?";
+            this.Order_ManagerSignUPFormLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.Order_ManagerLoginFormLink_LinkClicked);
+            // 
+            // Order_ManagerLoginButton
+            // 
+            this.Order_ManagerLoginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Order_ManagerLoginButton.Location = new System.Drawing.Point(123, 182);
+            this.Order_ManagerLoginButton.Name = "Order_ManagerLoginButton";
+            this.Order_ManagerLoginButton.Size = new System.Drawing.Size(122, 49);
+            this.Order_ManagerLoginButton.TabIndex = 5;
+            this.Order_ManagerLoginButton.Text = "Login";
+            this.Order_ManagerLoginButton.UseVisualStyleBackColor = true;
+            this.Order_ManagerLoginButton.Click += new System.EventHandler(this.Order_ManagerLoginButton_Click);
+            // 
+            // Order_ManagerPasswordText
+            // 
+            this.Order_ManagerPasswordText.Location = new System.Drawing.Point(186, 135);
+            this.Order_ManagerPasswordText.Name = "Order_ManagerPasswordText";
+            this.Order_ManagerPasswordText.Size = new System.Drawing.Size(154, 22);
+            this.Order_ManagerPasswordText.TabIndex = 6;
+            this.Order_ManagerPasswordText.TextChanged += new System.EventHandler(this.Order_ManagerPasswordText_TextChanged);
+            // 
+            // LoginOrder_ManagerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 314);
+            this.Controls.Add(this.Order_ManagerPasswordText);
+            this.Controls.Add(this.Order_ManagerLoginButton);
+            this.Controls.Add(this.Order_ManagerSignUPFormLink);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Order_ManagerLoginText);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoginOrder_ManagerForm";
+            this.Text = "Login as Order_Manager ";
+            this.Load += new System.EventHandler(this.LoginOrder_ManagerForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Order_ManagerLoginText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel Order_ManagerSignUPFormLink;
+        private System.Windows.Forms.Button Order_ManagerLoginButton;
+        private System.Windows.Forms.TextBox Order_ManagerPasswordText;
+    }
+}
---
+++ SignUpOrder_ManagerForm.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace CourseProjectWF
+{
+    partial class SignUpShipperForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.LinkToorder_managerLogin = new System.Windows.Forms.LinkLabel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.order_managerSignUpButtom = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(78, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(78, 343);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Description";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(78, 298);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Phone";
+            this.label3.Click += new System.EventHandler(this.label3_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(78, 255);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Address";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(78, 214);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "E-Mail";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(78, 169);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(106, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Password";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(285, 118);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(285, 155);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(285, 205);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(132, 22);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(285, 255);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(132, 22);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(285, 298);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(132, 22);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(285, 343);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(132, 22);
+            this.textBox6.TabIndex = 11;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // LinkToorder_managerLogin
+            // 
+            this.LinkToorder_managerLogin.AutoSize = true;
+            this.LinkToorder_managerLogin.Location = new System.Drawing.Point(148, 454);
+            this.LinkToorder_managerLogin.Name = "LinkToorder_managerLogin";
+            this.LinkToorder_managerLogin.Size = new System.Drawing.Size(173, 17);
+            this.LinkToorder_managerLogin.TabIndex = 12;
+            this.LinkToorder_managerLogin.TabStop = true;
+            this.LinkToorder_managerLogin.Text = "Already have an account?";
+            this.LinkToorder_managerLogin.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkToorder_managerLogin_LinkClicked);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(193, 45);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(106, 29);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Sign Up";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // order_managerSignUpButtom
+            // 
+            this.order_managerSignUpButtom.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.order_managerSignUpButtom.Location = new System.Drawing.Point(163, 395);
+            this.order_managerSignUpButtom.Name = "order_managerSignUpButtom";
+            this.order_managerSignUpButtom.Size = new System.Drawing.Size(136, 41);
+            this.order_managerSignUpButtom.TabIndex = 15;
+            this.order_managerSignUpButtom.Text = "Sign Up";
+            this.order_managerSignUpButtom.UseVisualStyleBackColor = true;
+            this.order_managerSignUpButtom.Click += new System.EventHandler(this.order_managerSignUpButtom_Click);
+            // 
+            // SignUporder_managerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(530, 489);
+            this.Controls.Add(this.order_managerSignUpButtom);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.LinkToorder_managerLogin);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SignUporder_managerForm";
+            this.Text = "Sign Up as order_manager";
+            this.Load += new System.EventHandler(this.SignUporder_managerForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.LinkLabel LinkToorder_managerLogin;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button order_managerSignUpButtom;
+    }
+}
